20f0031cbfbe4bdad553c95ccf91a6d3e78e2fcf
maycon souza <40838678+mikesouza1818@users.noreply.github.com>
2024-02-05 10:11:04 -0300

    Add files via upload
    Projeto: Consultor de Atividades com Base no Tempo
    
    Descrição:
    Este projeto consiste em um consultor de atividades que fornece sugestões com base nas condições climáticas. O programa solicita ao usuário que insira o estado atual do tempo (ensolarado, chuvoso ou nublado) e, com base nessa entrada, oferece recomendações de atividades apropriadas.
    
    Funcionamento:
    
    O programa inicia solicitando ao usuário que insira o estado atual do tempo.
    Utilizando estruturas condicionais (if, elif, else), o programa avalia a entrada do usuário e determina a condição correspondente.
    Com base na condição, o programa sugere uma atividade apropriada para o usuário desfrutar em relação ao clima.
    Se a entrada do usuário não corresponder a nenhum dos estados reconhecidos, o programa informa que não reconhece o tipo de tempo.
    
    This project is a weather-based activity consultant that provides suggestions based on current weather conditions. The program prompts the user to input the current weather state (sunny, rainy, or cloudy) and, based on this input, offers recommendations for suitable activities.
    
    Functionality:
    
    The program starts by asking the user to input the current weather state.
    Using conditional structures (if, elif, else), the program evaluates the user input and determines the corresponding condition.
    Based on the condition, the program suggests a suitable activity for the user to enjoy in relation to the weather.
    If the user's input does not match any of the recognized weather states, the program informs that it doesn't recognize the type of weather.

---
+++ projeto_tempo.py
@@ -0,0 +1,13 @@
+tempo=input('como está o tempo lá fora?(ensolarado/chuvoso/nublado)')
+
+if tempo=='ensolarado':
+    print('Vamos a praia!')
+
+elif tempo=='chuvoso':
+    print('fique em casa e assista um filme')
+
+elif tempo=='nublado':
+    print('talvez seja um bom dia para uma caminhada')
+
+else:
+    print('Desculpe,não reconheço esse tipo de tempo')
