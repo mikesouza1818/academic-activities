320c56dc7623eab5fbe759ad0bd0b80cd1902887
maycon souza <40838678+mikesouza1818@users.noreply.github.com>
2024-02-05 10:30:50 -0300

    Add files via upload
    Projeto: Comparação de Idades
    
    Descrição (Português):
    Este projeto em Python realiza a comparação de duas idades inseridas pelo usuário e informa qual delas é maior. O programa solicita que o usuário insira a idade 1 e a idade 2, e em seguida utiliza estruturas condicionais para determinar e exibir qual das idades é maior. Além disso, o programa identifica se as idades são iguais.
    
    Como Usar:
    
    Execute o programa em um ambiente Python.
    Digite a primeira idade quando solicitado.
    Digite a segunda idade quando solicitado.
    O programa imprimirá qual das idades é maior ou se são iguais.
    Project: Age Comparison
    
    Description (English):
    This Python project compares two ages entered by the user and informs which one is greater. The program prompts the user to input Age 1 and Age 2, and then uses conditional structures to determine and display which of the ages is greater. Additionally, the program identifies if the ages are equal.
    
    How to Use:
    
    Run the program in a Python environment.
    Enter the first age when prompted.
    Enter the second age when prompted.
    The program will print which age is greater or if they are equal.

---
+++ CompararIdades.py
@@ -0,0 +1,15 @@
+#Comparação de Idades:Peça ao usuário para inserir duas idades e informe qual delas é maior.
+
+idade1 = int(input('Digite a primeira idade:'))
+idade2 = int(input('Digite a segunda idade: '))
+
+if idade1> idade2:
+    print("a primeira idade é maior.")
+
+elif idade2>idade1:
+    print('a segunda idade é maior.')
+
+else:
+    print('As idades são iguais.')
+
+             
